a73625d333b022c9fed5f61bc1e1aaeed6012874
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-07-06 20:17:17 -0400

    add prototype map

---
+++ src/SearchBar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+  render() {
+    return(
+      <div>
+        <p> THe SearchBar stuffffffff</p>
+        <form>
+          <input>
+
+            
+          </input>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
