61044e9cd912cd52f199223694227c850e0b67b7
spencer kyonka <spencer.kyonka@hotmail.ca>
2018-07-09 12:30:40 -0400

    work on autocomplete

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import AutocompleteBox from './AutocompleteBox';
 import '../CSS/SearchBar.css';
+import CountryList from './CountryList';
+
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
     this.state = {
       letters: "",
+      autocompleteList: [],
       countryCode: ""
     }
     this.handleChange = this.handleChange.bind(this);
@@ -14,9 +17,34 @@ class SearchBar extends Component {
 
   handleChange(event) {
     event.preventDefault();
-    this.setState({ letters: event.target.value })
+    this.setState({ letters: event.target.value });
+    console.log(CountryList);
   }
 
+  // handleSubmit(event) {
+  //   event.preventDefault();
+  //
+  //   for (let i = 0; i < country_list.length; i++) {
+  //     if (letters.toLowerCase() == Object.keys(country_list)[i].toLowerCase()) {
+  //       this.state.countryCode = country_list[i];
+  //     }
+  //
+  //   }
+  //
+  //   // componentDidMount() {
+  //   //   axios.get(`
+  //   //     http://api.worldbank.org/v2/countries/wld/indicators/SP.POP.TOTL?format=json
+  //   //     `)
+  //   //   .then(res => {
+  //   //     this.setState({ info: res.data });
+  //   //     this.setState({ popper: res.data[1][0].value });
+  //   //     console.log(this.state.popper);
+  //   //
+  //   //   });
+  //   // }
+  //
+  //
+  // }
 
   render() {
     return(
@@ -29,14 +57,7 @@ class SearchBar extends Component {
 
         </form>
 
-
-
-
-
-
-
-
-        <AutocompleteBox letters={this.state.letters}/>
+        <AutocompleteBox letters={this.state.letters} countryList={CountryList}/>
       </div>
     );
   }
